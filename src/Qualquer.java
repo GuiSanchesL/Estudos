ab7ec7be27f89998d9ce03228bfe18b673bbce04
GuiSanchesL <guilhermesanchesloureiro@gmail.com>
2026-05-26 00:04:03 -0300

    Teminando vetores

---
+++ src/Qualquer.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Qualquer {
+    public static void main(String[] args) {
+        ArrayList<String> nomes=new ArrayList<>();
+        nomes.add ("Gabshow");
+        nomes.add ("Gui");
+        nomes.add ("luiza");
+        nomes.add("Anderson");
+        nomes.add("Flávia");
+        System.out.println(nomes.get(3));
+    }
+
+    }
+
