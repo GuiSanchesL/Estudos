47a3ece10307cf625a5eed85cfee8a5e06e8ec2b
GuiSanchesL <guilhermesanchesloureiro@gmail.com>
2026-05-26 00:24:32 -0300

    Teste Verde

---
+++ src/UsoDoArreylist.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UsoDoArreylist {
+
+
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        System.out.print("Em que ano Voçe nasceu ?:");
+        int Nascimento= teclado.nextInt();
+        int IdadeAtual=2026;
+
+        if (Nascimento-IdadeAtual<=18){
+            System.out.println("Maior de idade ");
+        }else {
+
+        }
+
+
+
+    }
+}
