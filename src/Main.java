6335b20726f5bf570a286427534bc637fc9170bf
GuiSanchesL <guilhermesanchesloureiro@gmail.com>
2026-06-01 21:49:05 -0300

    Uso simples do java.util , para uma lógica simples

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner teclado=new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Hoje falo com o senhor ? :");
+        String nome = teclado.next();
+        System.out.print("Quanto de saldo o senhor tem no banco:");
+        int saldo = teclado.nextInt();
+        System.out.println("Então o senhor " + nome + " tem R$:" + saldo + " de saldo ");
+        System.out.print("Quanto voçê gostaria de sacar: ?");
+        int saque= teclado.nextInt();
+        int resultado=saldo - saque;
+        System.out.println("Então o senhor vai ficar com o saldo de R$:"+resultado);
 
-        System.out.println("Qual o seu nome ");
-        teclado.next();
-        }
     }
+
+}
