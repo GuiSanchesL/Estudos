d4f199b020bca04d82d43fd2f19ed186f4072765
GuiSanchesL <guilhermesanchesloureiro@gmail.com>
2026-06-01 21:51:36 -0300

    Uso simples do java.util , para uma lógica simples

---
+++ src/SistemaBancario.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SistemaBancario {
+        public static void main(String[] args) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.print("Hoje falo com o senhor ? :");
+            String nome = teclado.next();
+            System.out.print("Quanto de saldo o senhor tem no banco:");
+            int saldo = teclado.nextInt();
+            System.out.println("Então o senhor " + nome + " tem R$:" + saldo + " de saldo ");
+            System.out.print("Quanto voçê gostaria de sacar: ?");
+            int saque= teclado.nextInt();
+            int resultado=saldo - saque;
+            System.out.println("Então o senhor vai ficar com o saldo de R$:"+resultado);
+
+        }
+
+    }
+
