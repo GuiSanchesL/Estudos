c7963661e8f437bb3f98fe7b880ce6bf0132c61d
GuiSanchesL <guilhermesanchesloureiro@gmail.com>
2026-06-01 22:01:56 -0300

    Uso simples do java.util , para uma lógica simples

--- src/TesteDeTudo.java
+++ src/TesteDeTudo.java
@@ -6,6 +6,7 @@ public class TesteDeTudo {
         System.out.println("Qual a Sua idade ?");
         int idade= teclado.nextInt();
         if (idade>=18&&idade<=70){
+            //uso do && como "E", Querendo dizer que 18 e 70 tem que cumprir o if
             System.out.println("Voto Obrigatorio");
         }else{
             System.out.println("Não vota");
