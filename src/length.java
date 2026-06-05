66ca5ca8622da7fe5696442f939e76d45b53073c
GuiSanchesL <guilhermesanchesloureiro@gmail.com>
2026-06-05 11:37:17 -0300

    Uso do array juntamente do for com .length

---
+++ src/length.java
@@ -0,0 +1,23 @@
+public class length {
+    public static void main(String[] args) {
+        int [][] filas= new int[4][4];
+
+        filas [0][1]=10;
+        filas [0][2]=20;
+        filas [0][3]=30;
+        filas [1][4]=40;
+
+        // Uso de dois [] sempre bom criar duas variaveis
+        // uso do .length para saber quantas posioçoes aquele array tem
+        //Diferente do Arraylist que é melhor quando voçê nao sabe o tamanho.
+        for (int p = 0; p < filas.length; p++)
+        { for (int c=0; c< filas[c].length; c++){
+            System.out.println(filas[p][c]);
+
+        }
+
+
+
+        }
+    }
+}
